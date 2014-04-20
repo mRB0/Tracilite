c39f92dc8ca41c864f68202819f750bb3ee1beea
Mike Burke <mrburke@gmail.com>
2014-04-20 19:38:07 -0300

    Initial commit of actual code.  Solution with two projects; one is the (C#) main program, and the other is a C++/CLI bridge that we'll use to call native code.  IronPython is also embedded; a simple script is being called to test the interface between Python, Tracilite, and TraciliteBridge.

---
+++ Tracilite/Form1.cs
@@ -0,0 +1,40 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tracilite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "New text";
+
+            label1.Text = "Some text";
+
+            RunScript();
+        }
+
+        public void RunScript()
+        {
+            ScriptEngine engine = Python.CreateEngine();
+            ScriptSource source = engine.CreateScriptSourceFromString("target.label1.Text = bridge.GetSomething()");
+            ScriptScope scope = engine.CreateScope();
+
+            TraciliteBridge.BridgeClass bridge = new TraciliteBridge.BridgeClass();
+
+            scope.SetVariable("target", this);
+            scope.SetVariable("bridge", bridge);
+            source.Execute(scope);
+        }
+    }
+}
