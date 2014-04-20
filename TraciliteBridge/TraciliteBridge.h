a5412fa9fc4b691cb1fd1acc4d6d67c1620e47b3
Mike Burke <mrburke@gmail.com>
2014-04-20 19:38:07 -0300

    Initial commit of actual code.  Solution with two projects; one is the (C#) main program, and the other is a C++/CLI bridge that we'll use to call native code.  IronPython is also embedded; a simple script is being called to test the interface between Python, Tracilite, and TraciliteBridge.

---
+++ TraciliteBridge/TraciliteBridge.h
@@ -0,0 +1,16 @@
+// tracilite-native.h
+
+#pragma once
+
+using namespace System;
+
+namespace TraciliteBridge {
+
+	public ref class BridgeClass
+	{
+	public:
+		String ^GetSomething() {
+			return "Hahaha!";
+		}
+	};
+}
